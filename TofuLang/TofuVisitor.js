004fc9e62deb5ac9aca82ab0f7ecfc8afd95721b
Bryan Kwong <bfkwong@calpoly.edu>
2020-08-01 13:38:09 -0700

    expression statement finished

--- TofuLang/TofuVisitor.js
+++ TofuLang/TofuVisitor.js
@@ -1,9 +1,11 @@
 const ast = require("./ast");
+const TofuParser = require('./AntlrFiles/tofuParser');
+
 
 class TofuVisitor {
 
     constructor(ctxTree) {
-        this.visitProgram(ctxTree.program());
+        this.ast = this.visitProgram(ctxTree.program());
     }
 
     visitProgram(ctx) {
@@ -59,19 +61,152 @@ class TofuVisitor {
 
         if (ctx) {
             let stmts = [];
-            if (Array.isArray(ctx)) {
-                for (let stmt of ctx) {
-                    let output = chooseStatementVisitor(stmt);
-                    stmts.push(output);
-                }
-            } else {
-                stmts.push(chooseStatementVisitor(ctx));
+            for (let stmt of ctx) {
+                stmts.push(chooseStatementVisitor(stmt));
             }
             return stmts;
         }
         return [];
     }
 
+    visitExpressionStatement(ctx) {
+        if (ctx.expStmt()) {
+            let expr = this.visitExpression(ctx.expStmt().expression());
+            return new ast.ST_EXP(expr);
+        }
+    }
+
+    visitExpression(ctx) {
+        let lhs = this.visitLogORExpression(ctx.logORExpression());
+        if (ctx.expression()) {
+            let rhs = this.visitExpression(ctx.expression());
+            return new ast.EXP_ASSIGN(lhs, rhs);
+        }
+        return lhs;
+    }
+
+    visitLogORExpression(ctx) {
+        if (ctx.logANDExpression(1)) {
+            let lft = this.visitLogANDExpression(ctx.logANDExpression(0));
+            let rht = this.visitLogANDExpression(ctx.logANDExpression(1));
+
+            return new ast.EXP_BINARY(new ast.BOP_OR, lft, rht);
+        }
+        return this.visitLogANDExpression(ctx.logANDExpression(0));
+    }
+
+    visitLogANDExpression(ctx) {
+        if (ctx.eqExpression(1)) {
+            let lft = this.visitEqExpression(ctx.eqExpression(0));
+            let rht = this.visitEqExpression(ctx.eqExpression(1));
+
+            return new ast.EXP_BINARY(new ast.BOP_AND, lft, rht);
+        }
+        return this.visitEqExpression(ctx.eqExpression(0))
+    }
+
+    visitEqExpression(ctx)  {
+        if (ctx.relExpression(1)) {
+            let lft = this.visitRelExpression(ctx.relExpression(0));
+            let rht = this.visitRelExpression(ctx.relExpression(1));
+            let opr = ctx.eqOp.text === "==" ? new ast.BOP_EQ : new ast.BOP_NE;
+
+            return new ast.EXP_BINARY(opr, lft, rht);
+        }
+        return this.visitRelExpression(ctx.relExpression(0));
+    }
+    
+    visitRelExpression(ctx) {
+        if (ctx.addExpression(1)) {
+            let lft = this.visitAddExpression(ctx.addExpression(0));
+            let rht = this.visitAddExpression(ctx.addExpression(1)); 
+            let opr; 
+            
+            switch (ctx.relOp.text) {
+                case ">":
+                    opr = new ast.BOP_GT;
+                    break;
+                case "<":
+                    opr = new ast.BOP_LT;
+                    break;
+                case ">=":
+                    opr = new ast.BOP_GE;
+                    break;
+                default:
+                    opr = new ast.BOP_LE;
+            }
+
+            return new ast.EXP_BINARY(opr, lft, rht);
+        }
+        return this.visitAddExpression(ctx.addExpression(0));
+    }
+
+    visitAddExpression(ctx) {
+        if (ctx.multExpression(1)) {
+            let lft = this.visitMultExpression(ctx.multExpression(0));
+            let rht = this.visitMultExpression(ctx.multExpression(1));
+            let opr = ctx.addOp.text === "+" ? new ast.BOP_PLUS : new ast.BOP_MINUS;
+
+            return new ast.EXP_BINARY(opr, lft, rht);
+        }
+        return this.visitMultExpression(ctx.multExpression(0));
+    }
+
+    visitMultExpression(ctx) {
+        if (ctx.unaryExpression(1)) {
+            let lft = this.visitUnaryExpression(ctx.unaryExpression(0));
+            let rht = this.visitUnaryExpression(ctx.unaryExpression(1));
+            let opr = ctx.multOp.text === "*" ? new ast.BOP_TIMES : new ast.BOP_DIVIDE;
+
+            return new ast.EXP_BINARY(opr, lft, rht);
+        }
+        return this.visitUnaryExpression(ctx.unaryExpression(0));
+    }
+
+    visitUnaryExpression(ctx) {
+        let opr = ctx.unaryOp ? (ctx.unaryOp.text === "!" ? new ast.UOP_NOT : new ast.UOP_MINUS) : undefined;
+        let expr = this.visitCallMemExpression(ctx.callMemExpression());
+        return opr ? new ast.EXP_UNARY(opr, expr) : expr;
+    }
+
+    visitCallMemExpression(ctx) {
+        return this.visitPrimaryExpression(ctx.primaryExpression());
+    }
+
+    visitPrimaryExpression(ctx) {
+        let expr;
+        switch(ctx.constructor.name) {
+            case "NumberExpressionContext":
+                let number = ctx.NUMBER().getText();
+                let numParsed = number.includes(".") ? parseFloat(number) : parseInt(number);
+                expr = new ast.EXP_NUM(numParsed);
+                break;
+            case "TrueExpressionContext":
+                expr = new ast.EXP_TRUE();
+                break;
+            case "FalseExpressionContext":
+                expr = new ast.EXP_FALSE();
+                break;
+            case "StringExpressionContext":
+                expr = new ast.EXP_STR(ctx.STRING().getText());
+                break;
+            case "UndefinedExpressionContext":
+                expr = new ast.EXP_UNDEFINED();
+                break;
+            case "IdentifierExpressionContext":
+                expr = new ast.EXP_ID(ctx.IDENTIFIER().getText());
+                break;
+            case "MakeExpressionContext":
+                expr = new ast.EXP_MAKE(ctx.IDENTIFIER().getText());
+                break;
+            default:
+                throw new Error("Error: Expression Type Unmatched");
+        }
+        return expr;
+    }
 }
+
+
+
 
 exports.TofuVisitor = TofuVisitor;
--- TofuLang/index.js
+++ TofuLang/index.js
@@ -4,7 +4,7 @@ const TofuParser = require('./AntlrFiles/tofuParser').tofuParser;
 
 const TofuVisitor = require('./TofuVisitor').TofuVisitor;
 
-const input = "x = 3;";
+const input = "3;";
 
 const chars = new antlr4.InputStream(input);
 const lexer = new TofuLexer(chars);
@@ -12,4 +12,7 @@ const lexer = new TofuLexer(chars);
 const tokens = new antlr4.CommonTokenStream(lexer);
 const parser = (new TofuParser(tokens));
 
-new TofuVisitor(parser);
+const visitor = new TofuVisitor(parser);
+console.log(visitor.ast.stmts);
+
+const
