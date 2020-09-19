2e9ab16e415a87603ec94b1e902661511316ab38
Bryan Kwong <bfkwong@calpoly.edu>
2020-09-19 16:16:23 -0700

    chore: created new dev interface

--- TofuLang/index.js
+++ TofuLang/index.js
@@ -1,25 +1,36 @@
-const fs = require('fs');
-const antlr4 = require('antlr4');
-const util = require('util');
-const TofuLexer = require('./AntlrFiles/tofuLexer').tofuLexer;
-const TofuParser = require('./AntlrFiles/tofuParser').tofuParser;
+const fs = require("fs");
+const readline = require("readline");
+const antlr4 = require("antlr4");
+const TofuLexer = require("./AntlrFiles/tofuLexer").tofuLexer;
+const TofuParser = require("./AntlrFiles/tofuParser").tofuParser;
+const TofuVisitor = require("./TofuVisitor").TofuVisitor;
+const TofuEvaluator = require("./TofuEvaluator").TofuEvaluator;
 
-const TofuVisitor = require('./TofuVisitor').TofuVisitor;
-const TofuEvaluator = require('./TofuEvaluator').TofuEvaluator;
-
-let input;
-if (process.argv[3]) {
-    input = fs.readFileSync(`./SampleCode/${process.argv[2]}.tofu`, 'utf8');
-} else {
-    input = fs.readFileSync(`./SampleCode/${4}.tofu`, 'utf8');
+function scripter(input) {
+  const chars = new antlr4.InputStream(input);
+  const lexer = new TofuLexer(chars);
+  const tokens = new antlr4.CommonTokenStream(lexer);
+  const parser = new TofuParser(tokens);
+  const visitor = new TofuVisitor(parser);
+  new TofuEvaluator(visitor.ast);
 }
 
-const chars = new antlr4.InputStream(input);
-const lexer = new TofuLexer(chars);
-
-const tokens = new antlr4.CommonTokenStream(lexer);
-const parser = (new TofuParser(tokens));
-
-const visitor = new TofuVisitor(parser);
-
-const eval = new TofuEvaluator(visitor.ast);
+if (process.argv[2]) {
+  const input = fs.readFileSync(`./SampleCode/code/${process.argv[2]}`, "utf8");
+  scripter(input);
+} else {
+  const input = [];
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.prompt();
+  rl.on("line", function (cmd) {
+    input.push(cmd);
+  });
+  rl.on("close", function (cmd) {
+    const code = input.join("\n");
+    scripter(code);
+    process.exit(0);
+  });
+}
